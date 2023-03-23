6d4eed9807da95e10e036afae021a0646cd64958
Dhyey Kathiriya <dhk22500@gmail.com>
2023-03-23 12:24:58 +0530

    added router for all practicals display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { P11Component } from './p11/p11.component';
+import { P12Component } from './p12/p12.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Prac11', component: P11Component},
+  {path:'Prac12', component: P12Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{useHash: true})],
