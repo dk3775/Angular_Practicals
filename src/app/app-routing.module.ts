81099b43750b18e1411ef6eba30bf15f1384d9f4
Dhyey Kathiriya <dhk22500@gmail.com>
2023-03-28 10:31:28 +0530

    testing auto build deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { P11Component } from './p11/p11.component';
 import { P12Component } from './p12/p12.component';
+import { P13MainComponent } from './p13-main/p13-main.component';
 
 const routes: Routes = [
   {path:'Prac11', component: P11Component},
   {path:'Prac12', component: P12Component},
+  {path:'Prac13', component: P13MainComponent},
 ];
 
 @NgModule({
